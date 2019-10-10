6b98631207f9b69d80bee8fa7351ed0c7aafa0b0
Bharad <bharadwaj6991@gmail.com>
2019-10-10 13:14:19 +0800

    Implemented Merge sort and Insertion sort using the pseudocode from our lecture notes.

--- Lab3.py
+++ Lab3.py
@@ -1,10 +1,11 @@
 import random
+from time import process_time
 
-def merge(arr, n, m):
+
+def merge(arr, n, mid, m):
     if m - n <= 0:
         return
 
-    mid = (n + m) // 2
     right_half_index = mid + 1
     left_half_index = n
 
@@ -37,11 +38,6 @@ def merge(arr, n, m):
             right_half_index += 1
             mid += 1
 
-# arr = [2, 4, 3, 5]
-# print(arr)
-# merge(arr, 0, 3)
-# print(arr)
-
 def mergeSort(arr, n, m):
     mid = (n + m) // 2
     if(m - n <= 0):
@@ -49,16 +45,48 @@ def mergeSort(arr, n, m):
     else:
         mergeSort(arr, n, mid)
         mergeSort(arr, mid + 1, m)
-    merge(arr, n, m)
+    merge(arr, n, mid, m)
+
+# # Simple test to check if merge sort is working
+# arr = [i for i in range(20)]
+# print(arr)
+# random.shuffle(arr)
+# print(arr)
+# mergeSort(arr, 0, len(arr) - 1)
+# print(arr)
+
 
 def swap(index1, index2, arr):
     temp = arr[index1]
     arr[index1] = arr[index2]
     arr[index2] = temp
 
-def insertionSort(arr):
+# def basic_insertionSort(arr):
+#     num_of_comparisons = 0
+#     for i in range(1, len(arr)):
+#         for j in range(i, 0, -1):
+#             # print(i, j)
+#             num_of_comparisons += 1
+#             if arr[j] < arr[j - 1]:
+#                 # print(arr, num_of_comparisons)
+#                 swap(j, j - 1, arr)
+#                 # print(arr)
+#             else:
+#                 break
+#
+#     return num_of_comparisons
+#
+# # Simple test to check if insertion sort is working
+# arr = [i for i in range(20)]
+# print(arr)
+# random.shuffle(arr)
+# print(arr)
+# num_comparisons = basic_insertionSort(arr)
+# print(arr, num_comparisons)
+
+def insertionSort(arr, first, last):
     num_of_comparisons = 0
-    for i in range(1, len(arr)):
+    for i in range(first + 1, last + 1):
         for j in range(i, 0, -1):
             # print(i, j)
             num_of_comparisons += 1
@@ -72,19 +100,39 @@ def insertionSort(arr):
     return num_of_comparisons
 
 
-# # Simple test to check if merge sort is working
 # arr = [i for i in range(20)]
 # print(arr)
 # random.shuffle(arr)
 # print(arr)
-# mergeSort(arr, 0, len(arr) - 1)
-# print(arr)
-
-
-# # Simple test to check if insertion sort is working
-# arr = [i for i in range(20)]
-# print(arr)
-# random.shuffle(arr)
-# print(arr)
-# num_comparisons = insertionSort(arr)
+# num_comparisons = insertionSort(arr, 10, len(arr) - 1)
 # print(arr, num_comparisons)
+#
+
+
+def merge_insertion_sort(arr, first, last, switch_sort_num):
+
+    if last - first > switch_sort_num:
+        mid = first + last // 2
+        merge_insertion_sort(arr, first, mid, switch_sort_num)
+        merge_insertion_sort(arr, mid + 1, last, switch_sort_num)
+        merge(arr, first, mid, last)
+
+    else:
+        insertionSort(arr, first, last)
+
+
+start = process_time()
+
+arr_length = 20
+arr = [i for i in range(arr_length)]
+print(arr)
+random.shuffle(arr)
+print(arr)
+merge_insertion_sort(arr, 0, arr_length - 1, 10)
+print(arr)
+
+
+stop = process_time()
+print("Elapsed time:", stop, start)
+
+print("Elapsed time during the whole program in seconds:", stop - start)
