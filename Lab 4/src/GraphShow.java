aae06d10035c5456319933398d6d697cdb32592d
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2019-11-07 01:09:01 +0800

    Update GraphShow.java

--- Lab 4/src/GraphShow.java
+++ Lab 4/src/GraphShow.java
@@ -12,7 +12,7 @@ public class GraphShow {
         Graph graph = new Graph(false);
         BFS bfs = new BFS();
 //---------------Simple Menu---------------------		
-        int choice, departure, arrival;
+        int choice, departure, arrival, connectivity;
         
         Scanner sc = new Scanner(System.in);
         ArrayList<Node> nodes = new ArrayList<Node>();//used for printing out choices for user
@@ -38,15 +38,7 @@ public class GraphShow {
 			DataSets.createSet40(graph, nodes);
 		}
 		
-		//To create edge from 1 node to all nodes.
-//		for (Node i : nodes) {
-//			for (Node j : nodes) {
-//				 if(!graph.hasEdge(i, j) && i != j) {
-//					 graph.addEdge(i, j);
-//				 }
-//			}
-//			
-//		}
+
 		System.out.println("These are the Cities Available:");
 		for (Node node : nodes) {
 			System.out.println(node.id+1 + ". " + node.name);
@@ -56,10 +48,248 @@ public class GraphShow {
 		System.out.print("Enter Arrival City: ");
 		arrival = sc.nextInt();
 		
-		//To display the calculate and display shortest path
-		startTime = System.nanoTime();
-		bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
-		endTime = System.nanoTime();
-		System.out.println("Time taken = " + ((endTime-startTime)/1000) + "ms");
+		//To display the calculate and display shortest path - Original		
+		long totaltime = 0;
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: Original");
+        
+	    
+		//To create edge from 1 node to all nodes.
+	    connectivity = graph.getAdjacencyList().size()/100*10;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 10%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*20;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 20%");
+	    
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*30;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 30%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*40;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 40%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*50;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 50%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*60;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 60%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*70;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 70%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*80;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 80%");
+	    
+	    connectivity = graph.getAdjacencyList().size()/100*90;
+      	for (Node i : nodes) {
+      		if (connectivity!=0) {
+	      		connectivity--;
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+      		}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 90%");
+	    
+
+      	for (Node i : nodes) {
+				for (Node j : nodes) {
+					 if(!graph.hasEdge(i, j) && i != j) {
+						 graph.addEdge(i, j);
+					 }
+				}
+		}
+	    for(int i = 0 ;i<5;i++){
+	        startTime = System.nanoTime();
+	        bfs.shortestRoute(graph, nodes.get(departure-1), nodes.get(arrival-1));
+	        endTime = System.nanoTime();
+	        if(i==0)
+	            totaltime=endTime-startTime;
+	        else
+	            totaltime=endTime-startTime + totaltime;
+	    }
+	    System.out.println("Time taken = " + ((totaltime/5)/1000) + "ms");
+	    System.out.println("Connectivity: 100%");
     }
 }
