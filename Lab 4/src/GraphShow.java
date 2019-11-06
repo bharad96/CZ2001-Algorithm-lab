6789d2364940602f9b4eb550a55ed366c9f89f42
xiaochaorenn <54619857+xiaochaorenn@users.noreply.github.com>
2019-11-06 22:37:27 +0800

    Update GraphShow.java

--- Lab 4/src/GraphShow.java
+++ Lab 4/src/GraphShow.java
@@ -10,44 +10,8 @@ public class GraphShow {
     	long startTime, endTime;
     	
         Graph graph = new Graph(false);
-        Node a = new Node(0, "Beijing");
-        Node b = new Node(1, "Hong Kong");
-        Node c = new Node(2, "Singapore");
-        Node d = new Node(3, "Shanghai");
-        Node e = new Node(4, "Tokyo");
-        Node f = new Node(5, "Seoul");
-        Node g = new Node(6, "Los Angeles");
-        Node h = new Node(7, "Sydney");
-
-        graph.addEdge(a,b);
-        graph.addEdge(a,c);
-        graph.addEdge(a,d);
-        graph.addEdge(a,e);
-        graph.addEdge(b,c);
-        graph.addEdge(b,d);
-        graph.addEdge(c,d);
-        graph.addEdge(c,e);
-        graph.addEdge(c,f);
-        graph.addEdge(c,h);
-        graph.addEdge(d,e);
-        graph.addEdge(d,f);
-        graph.addEdge(e,f);
-        graph.addEdge(e,g);
-        graph.addEdge(f,g);
-        
-        graph.printEdges();
-
-//      System.out.println(graph.hasEdge(a,b));
-//      System.out.println(graph.hasEdge(d,a));
-        
         BFS bfs = new BFS();
-		bfs.breadthFirstSearch(graph, a);
-	
-		
-//---------------Simple Menu---------------------
-		//time = System.nanoTime();
-		//time2 = System.nanoTime();
-		
+//---------------Simple Menu---------------------		
         int choice, departure, arrival;
         
         Scanner sc = new Scanner(System.in);
